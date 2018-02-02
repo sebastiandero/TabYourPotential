0d3c275e688c6d2e6f54fe666d0a0e9d339e02ec
Sebastian De Ro <sebastian.dero@yahoo.de>
2018-02-02 16:53:21 +0100

    removeAllTabs

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,8 +2,53 @@ browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-browser.browserAction.setBadgeText({
-  text: `'Allo`
-})
+let saveTabs = (contextName) => {
+  browser.tabs.query({}).then(arr => saveForContext(arr, contextName));
+};
 
-console.log(`'Allo 'Allo! Event Page for Browser Action`)
+let loadTabs = () => {
+  tabs.forEach(tab => {
+    browser.tabs.create({
+      url: tab.url
+    });
+  });
+};
+
+let saveForContext = (tabsToSave, contextName) => {
+
+  let tabsContext = {
+    tabs: tabsToSave,
+    name: contextName
+  };
+
+  let storageWrapper = JSON.parse(localStorage.getItem('TYP_StorageWrapper'));
+
+  if (storageWrapper) {
+    storageWrapper.contexts[contextName] = tabsToSave;
+  } else {
+    storageWrapper = {
+      contexts: {}
+    };
+
+    storageWrapper.contexts[contextName] = tabsToSave;
+  }
+
+  localStorage.setItem('TYP_StorageWrapper', JSON.stringify(storageWrapper));
+}
+
+let reloadTabs = () => {
+  let storageWrapper = JSON.parse(localStorage.getItem('TYP_StorageWrapper'));
+
+  if (storageWrapper) {
+    browser.tabs
+  }
+};
+
+let removeAllTabs = () => {
+  return new Promise((resolve, reject) => {
+
+    browser.tabs.query({}).then(arr => {
+      browser.tabs.discard(arr.map(tab => tab.id));
+    });
+  });
+}
